ec5f7fbff295ae2bd8fef7fd04453dfd9e659249
Anthony Laibe <anthony@laibe.cc>
2017-12-26 15:40:24 +0000

    Update ruby.zsh

--- zsh/config/ruby.zsh
+++
@@ -1,8 +0,0 @@
-function rbinstall() {
-  command ruby-install ruby $(cat .ruby-version) > /dev/null
-  gem install bundler pry
-}
-
-eval "$(rbenv init --no-rehash - zsh)"
-
-alias rubies="rbenv versions"
