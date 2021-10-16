738778dcd24d050c352e5dfecb6c842eec91bcaf
Anthony Laibe <anthony@laibe.cc>
2021-10-16 17:45:21 +0200

    Add spacemacs config

--- setup.sh
+++ setup.sh
@@ -24,6 +24,7 @@ ln -s $HOME/dotfiles/vim/vimrc .vimrc
 ln -s $HOME/dotfiles/zsh/zshrc .zshrc
 ln -s $HOME/dotfiles/bin bin
 ln -s $HOME/dotfiles/nvim/init.vim .config/nvim/init.vim
+ln -s $HOME/dotfiles/emacs/spacemacs .spacemacs
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.0
