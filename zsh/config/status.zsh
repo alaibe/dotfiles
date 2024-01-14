c4823e6a3e124f867305fb2f5c0c497dc9b2476f
Anthony Laibe <anthony@laibe.cc>
2024-01-14 14:59:10 +0100

    update status config

--- zsh/config/status.zsh
+++ zsh/config/status.zsh
@@ -1,3 +1,3 @@
-export PATH=$PATH:/opt/go/bin:~/go/bin
-export PATH=/opt/qt/5.14.2/gcc_64/bin:$PATH
-export QTDIR=/opt/qt/5.14.2/gcc_64
+export PATH=$PATH:~/go/bin
+export PATH=/opt/qt/5.15.2/gcc_64/bin:$PATH
+export QTDIR=/opt/qt/5.15.2/gcc_64
